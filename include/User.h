12f4ced944350d2a269ef9138d24b5272c1bb403
sampagavi <sampagavivinuta.05@gmail.com>
2025-06-05 15:52:30 +0530

    Update User.h

--- include/User.h
+++ include/User.h
@@ -1,4 +1,4 @@
-#ifndef USER_H
+/*#ifndef USER_H
 #define USER_H
 
 #include <string>
@@ -12,4 +12,19 @@ public:
     User(int _id, std::string _name, std::string _contact);
 };
 
-#endif
+#endif*/
+#ifndef USER_H
+#define USER_H
+
+#include <string>
+
+class User {
+public:
+    int id;
+    std::string name;
+    std::string contact;
+
+    User(int _id, const std::string& _name, const std::string& _contact);
+};
+
+#endif 
