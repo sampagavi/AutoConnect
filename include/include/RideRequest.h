839a67737b3ae0082dac42b53ac569237c28137d
sampagavi <sampagavivinuta.05@gmail.com>
2025-06-05 15:15:20 +0530

    Create RideRequest.h

---
+++ include/include/RideRequest.h
@@ -0,0 +1,22 @@
+#ifndef RIDEREQUEST_H
+#define RIDEREQUEST_H
+
+#include <string>
+
+enum UrgencyLevel { LOW, MEDIUM, HIGH };
+
+class RideRequest {
+public:
+    int requestId;
+    int studentId;
+    std::string pickupLocation;
+    std::string destination;
+    UrgencyLevel urgency;
+    bool isHandled;
+    std::string feedback;
+
+    RideRequest(int _requestId, int _studentId, std::string _pickup,
+                std::string _dest, UrgencyLevel _urgency);
+};
+
+#endif
