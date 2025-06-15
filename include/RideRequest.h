27a1861d343789c3e494a4c96d5bf155772ade65
sampagavi <sampagavivinuta.05@gmail.com>
2025-06-15 22:10:20 +0530

    Update RideRequest.h

--- include/RideRequest.h
+++ include/RideRequest.h
@@ -9,9 +9,15 @@ struct RideRequest {
     Location location;
     int urgency;
 
-    RideRequest(std::string name, Location loc, int urg);
-    bool operator<(const RideRequest& other) const;
+    RideRequest(std::string name, Location loc, int urg)
+        : studentName(name), location(loc), urgency(urg) {}
+
+    // Higher urgency gets higher priority. If urgency is same, shorter distance is preferred.
+    bool operator<(const RideRequest& other) const {
+        return urgency < other.urgency;
+    }
 };
 
 #endif
+
 
