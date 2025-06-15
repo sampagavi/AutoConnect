6716c132f8d50b419c54bd2bfe8cd5633d38d225
sampagavi <sampagavivinuta.05@gmail.com>
2025-06-15 13:21:05 +0530

    Update RideRequest.h

--- include/RideRequest.h
+++ include/RideRequest.h
@@ -1,46 +1,17 @@
-/*#ifndef RIDEREQUEST_H
-#define RIDEREQUEST_H
-
-#include <string>
-
-enum UrgencyLevel { LOW, MEDIUM, HIGH };
-
-class RideRequest {
-public:
-    int requestId;
-    int studentId;
-    std::string pickupLocation;
-    std::string destination;
-    UrgencyLevel urgency;
-    bool isHandled;
-    std::string feedback;
-
-    RideRequest(int _requestId, int _studentId, std::string _pickup,
-                std::string _dest, UrgencyLevel _urgency);
-};
-
-#endif*/
-
 #ifndef RIDEREQUEST_H
 #define RIDEREQUEST_H
 
 #include <string>
+#include "User.h"
 
-enum UrgencyLevel { LOW, MEDIUM, HIGH };
+struct RideRequest {
+    std::string studentName;
+    Location location;
+    int urgency;
 
-class RideRequest {
-public:
-    int requestId;
-    int studentId;
-    std::string pickupLocation;
-    std::string destination;
-    UrgencyLevel urgency;
-    bool isHandled;
-    std::string feedback;
-
-    RideRequest(int _requestId, int _studentId, const std::string& _pickup,
-                const std::string& _dest, UrgencyLevel _urgency);
+    RideRequest(std::string name, Location loc, int urg);
+    bool operator<(const RideRequest& other) const;
 };
 
-#endif 
+#endif
 
