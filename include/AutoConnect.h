7d1bb6527b4f3a6698df1200fa5225fdb85178c3
sampagavi <sampagavivinuta.05@gmail.com>
2025-06-15 22:12:13 +0530

    Update AutoConnect.h

--- include/AutoConnect.h
+++ include/AutoConnect.h
@@ -1,9 +1,12 @@
+
+
 #ifndef AUTOCONNECT_H
 #define AUTOCONNECT_H
+
+#include "GeoUtils.h"  // or "User.h", depending on where Location is defined
 
 void studentRequestRide();
 void driverAcceptRide();
 Location inputLocation();
 
 #endif
-
