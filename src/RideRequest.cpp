6906bf135c3fceeba04c9d5bf99fdceb10272ddf
sampagavi <sampagavivinuta.05@gmail.com>
2025-06-15 13:23:32 +0530

    Update RideRequest.cpp

--- src/RideRequest.cpp
+++ src/RideRequest.cpp
@@ -1,23 +1,9 @@
-/*#include "RideRequest.h"
-
-RideRequest::RideRequest(int requestId, const std::string& pickupLocation, const std::string& dropoffLocation)
-    : requestId(requestId), pickupLocation(pickupLocation), dropoffLocation(dropoffLocation) {}
-
-int RideRequest::getRequestId() const {
-    return requestId;
-}
-
-std::string RideRequest::getPickupLocation() const {
-    return pickupLocation;
-}
-
-std::string RideRequest::getDropoffLocation() const {
-    return dropoffLocation;
-}*/
-
 #include "RideRequest.h"
 
-RideRequest::RideRequest(int _requestId, int _studentId, const std::string& _pickup,
-                         const std::string& _dest, UrgencyLevel _urgency)
-    : requestId(_requestId), studentId(_studentId), pickupLocation(_pickup),
-      destination(_dest), urgency(_urgency), isHandled(false), feedback("") {}
+RideRequest::RideRequest(std::string name, Location loc, int urg)
+    : studentName(name), location(loc), urgency(urg) {}
+
+bool RideRequest::operator<(const RideRequest& other) const {
+    return urgency < other.urgency; // Higher urgency = higher priority
+}
+
