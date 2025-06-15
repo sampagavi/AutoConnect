3b7cec68aa026586366c4533545b9e2ceabfe324
sampagavi <sampagavivinuta.05@gmail.com>
2025-06-15 22:13:48 +0530

    Update RideRequest.cpp

--- src/RideRequest.cpp
+++ src/RideRequest.cpp
@@ -4,6 +4,5 @@ RideRequest::RideRequest(std::string name, Location loc, int urg)
     : studentName(name), location(loc), urgency(urg) {}
 
 bool RideRequest::operator<(const RideRequest& other) const {
-    return urgency < other.urgency; // Higher urgency = higher priority
+    return urgency < other.urgency; 
 }
-
